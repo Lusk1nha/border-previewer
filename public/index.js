87ba1e379c253902b300e15139e5ef230d21afac
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2021-02-26 04:30:48 -0300

    unit-mode

--- public/index.js
+++ public/index.js
@@ -1,37 +1,57 @@
 const allInputs = document.querySelectorAll('#inputs-container input')
+const allUnits = document.querySelectorAll('#unit-buttons .unit')
+
 const inputAllSides = document.querySelector('#allInputs')
 
-inputAllSides.addEventListener('input', allSides)
-for ( input of allInputs ) inputFunctionalities(input)
+console.log(allUnits)
 
-function inputFunctionalities(input) {
-  input.addEventListener('input', updateSquare)
+let valueUnit = 'px'
 
-  function updateSquare(e) {
-    inputAllSides.value = ''
-    inputAllSides.placeholder = 'Custom'
+allUnits.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.classList[1] != valueUnit) changeUnit(button)
+  })
+})
 
-    const squareBlock = document.querySelector('#squareBlock')
+inputAllSides.addEventListener('input', changeAllSides)
 
-    const inputSide = e.target.classList[0]
-    const inputValue = e.target.value
+allInputs.forEach(input => input.addEventListener('input', updateSquare))
 
-    const borderSide = `border${inputSide}Radius`
+function updateSquare(e) {
+  inputAllSides.value = ''
+  inputAllSides.placeholder = 'Custom'
 
-    return squareBlock.style[borderSide] = `${inputValue}px`
-  }
+  const inputSide = e.target.classList[0]
+  const inputValue = e.target.value
+
+  const borderSide = `border${inputSide}Radius`
+
+  return squareBlock.style[borderSide] = `${inputValue}${valueUnit}`
 }
 
-function allSides() {
+function changeAllSides() {
   allInputs.forEach(input => {
     const inputAllValue = inputAllSides.value
-    inputAllSides.placeholder = 'All sides'
+    inputAllSides.placeholder = `0${valueUnit}`
   
     const inputSide = input.classList[0]
     const borderSide = `border${inputSide}Radius`
     
     input.value = inputAllValue
-    return squareBlock.style[borderSide] = `${inputAllValue}px`
+    return squareBlock.style[borderSide] = `${inputAllValue}${valueUnit}`
 
   })
+}
+
+function changeUnit(unit) {
+  allUnits.forEach(button => button.classList.remove('active'))
+
+  valueUnit = unit.classList[1]
+  
+  allInputs.forEach(input => input.placeholder = `0${valueUnit}`)
+  inputAllSides.placeholder = `0${valueUnit}`
+
+  changeAllSides()
+  
+  return unit.classList.add('active')
 }
